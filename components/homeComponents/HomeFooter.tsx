032e6002313f3b9fc041ea75052092d40612d6a1
nova_44056 <senguy44056@gmail.com>
2024-04-04 15:59:33 -0500

    feat: Update Home Footer

--- components/homeComponents/HomeFooter.tsx
+++ components/homeComponents/HomeFooter.tsx
@@ -1,69 +1,89 @@
 import TwitterIcon from '@mui/icons-material/Twitter';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import FacebookIcon from '@mui/icons-material/Facebook';
+import React, { SVGProps } from 'react';
 
 export default function HomeFooter() {
   return (
-    <section className=" mt-16 px-6 py-8 md:text-base text-xs">
-      <hr className="my-4 bg-complementary" />
-      <div className="flex flex-col items-center justify-center gap-2 text-complementary text-center">
-        <div className="text-base md:text-lg">
-          {' '}
-          {/* !change */}
-          Checkout HackUTD&apos;s{' '}
-          <a
-            href="https://acmutd.co/"
-            rel="noopener noreferrer"
-            target="_blank"
-            className="font-black hover:underline"
-          >
-            Organizer website
-          </a>
+    <section style={{ minHeight: '387px' }} className=" mt-16 md:text-base text-xs relative">
+      <hr className=" bg-complementary" />
+      <div className="flex">
+        <Blob width={400} className="absolute hidden left-0 top-0 lg:block" />
+        <div className="z-10 grid grid-cols-1 lg:gap-36 lg:grid-cols-3">
+          <div className="lg:text-white p-10">
+            <h1 className="font-bold text-2xl mt-10">HackPortal</h1>
+            <p style={{ maxWidth: '300px' }} className="mt-1">
+              Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit
+              officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud
+              amet.
+            </p>
+            {/* social media here */}
+          </div>
+          <div className="p-10" style={{ minWidth: '400px', fontFamily: 'inter' }}>
+            <h1 className="font-semibold text-xl mb-3">Learn more</h1>
+            <div className="font-light">
+              <p className="mb-2">
+                Check out HackUTD’s <span className="font-semibold">organizer website</span>
+              </p>
+              <p className="mb-2">
+                Designed by <span className="font-semibold">HackUTD</span>
+              </p>
+              <p className="mb-2">
+                HackPortal developed with {'<3'} <span className="font-semibold">HackUTD </span>
+                and <span className="font-semibold">ACM Development</span>
+              </p>
+              <p className="mb-2">Source Code</p>
+            </div>
+          </div>
+          <div className="p-10" style={{ maxWidth: '300px', fontFamily: 'inter' }}>
+            <h1 className="font-semibold text-xl mb-3">Contact Us</h1>
+            {/* input for email */}
+            <input
+              style={{ backgroundColor: '#E2E2E2' }}
+              className="border-0 rounded"
+              placeholder="Email"
+              type="text"
+              name="email"
+              id="contact-us"
+            />
+            {/* subscribe button */}
+            <button
+              className="rounded-lg text-white px-6 py-2 my-4"
+              style={{ backgroundColor: '#7B81FF' }}
+            >
+              Subscribe
+            </button>
+          </div>
         </div>
-        <div className="text-[0.6rem] md:text-sm">
-          Designed by <p className="font-black inline">HackUTD | </p>
-          {/* PLEASE DO NOT CHANGE <3 */}
-          HackPortal developed with &lt;3 by <p className="font-black inline">HackUTD</p> and{' '}
-          <p className="font-black inline">ACM Development</p>
-          {/* PLEASE DO NOT CHANGE <3 */}
-        </div>
-        <div className="flex flex-row justify-center items-center space-x-6">
-          {/* !change */}
-          <a
-            href="mailto:email@organization.com"
-            rel="noopener noreferrer"
-            target="_blank"
-            className="hover:underline md:mr-8"
-          >
-            Contact Us
-          </a>
-          {/* !change */}
-          <a
-            href="https://github.com/acmutd/hackportal"
-            target="_blank"
-            rel="noreferrer"
-            className="hover:underline  whitespace-nowrap"
-          >
-            Source Code
-          </a>
-        </div>
-        {/* Social icons */} {/* !change */}
-        <div className="space-x-8 > * + *">
-          <a href="https://twitter.com/hackutd" rel="noopener noreferrer" target="_blank">
-            <TwitterIcon className="footerIcon" />
-          </a>
-          <a
-            href="https://www.instagram.com/hackutd/?hl=en"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            <InstagramIcon className="footerIcon" />
-          </a>
-          <a href="https://www.facebook.com/hackutd/" rel="noopener noreferrer" target="_blank">
-            <FacebookIcon className="footerIcon" />
-          </a>
-        </div>
+      </div>
+      <div
+        // hardcoded background color for now, but will upgrade to tailwind later
+        style={{ height: '37px', width: '100%', backgroundColor: '#7B81FF' }}
+        className="absolute bottom-0 flex items-center justify-center"
+      >
+        <h2 className="text-center text-white">All Copyrights are reserved by HackUTD</h2>
       </div>
     </section>
   );
 }
+
+// blob
+const Blob: React.FC<SVGProps<SVGSVGElement>> = (props) => {
+  return (
+    <svg
+      width="480"
+      height="385"
+      viewBox="0 0 480 385"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M38.9788 597.017C129.555 628.73 243.207 655.052 315.874 585.996C387.307 518.112 331.301 394.497 355.612 296.116C385.101 176.784 513.977 73.4295 470.3 -38.9127C425.588 -153.916 283.623 -190.831 166.515 -190.256C63.0498 -189.748 -30.1812 -121.127 -98.5997 -36.1217C-153.192 31.7049 -151.839 124.432 -157.987 212.914C-163.26 288.808 -164.756 363.637 -130.957 429.549C-92.3463 504.843 -37.3878 570.279 38.9788 597.017Z"
+        fill="#7B81FF"
+      />
+    </svg>
+  );
+};
