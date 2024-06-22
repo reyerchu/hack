dcc69387e95a48fd1104a4ac300e8f7a48a1418d
vvyn <vicavyn3@gmail.com>
2024-06-21 22:45:39 -0500

    feat(speakers): Resize speaker cards
    Added links and resized speaker cards

--- components/homeComponents/HomeSpeakers.tsx
+++ components/homeComponents/HomeSpeakers.tsx
@@ -10,11 +10,12 @@ export default function HomeSpeakers(props: { keynoteSpeakers: KeynoteSpeaker[] 
 
   useEffect(() => {
     setSpeakers(props.keynoteSpeakers);
+    console.log(speakers);
   }, []);
 
   return (
     speakers.length != 0 && (
-      <section className=" overflow-x-auto min-h-[24rem]">
+      <section className=" overflow-x-auto min-h-[24rem] bg-[#F2F3FF] pb-20">
         <div className="text-center text-4xl md:text-5xl font-bold text-[#05149C] pt-8 pb-2 font-fredoka">
           Speakers
         </div>
@@ -22,50 +23,60 @@ export default function HomeSpeakers(props: { keynoteSpeakers: KeynoteSpeaker[] 
           The members that make HackPortal possible
         </div>
 
-        <Swiper
-          navigation={true}
-          modules={[Navigation]}
-          className="mySwiper"
-          slidesPerView={1}
-          spaceBetween={10}
-          // Responsive breakpoints
-          breakpoints={{
-            // when window width is >= 320px
-            320: {
-              slidesPerView: 1,
-              spaceBetween: 10,
-            },
-            // when window width is >= 480px
-            480: {
-              slidesPerView: 1,
-              spaceBetween: 20,
-            },
-            620: {
-              slidesPerView: 2,
-              spaceBetween: 30,
-            },
-            // when window width is >= 640px
-            840: {
-              slidesPerView: 2,
-              spaceBetween: 40,
-            },
-            1024: {
-              slidesPerView: 3,
-              spaceBetween: 20,
-            },
-          }}
-        >
-          {speakers.map(({ name, subtitle, description, fileName }, idx) => (
-            <SwiperSlide key={idx}>
-              <KeynoteSpeaker
-                name={name}
-                subtitle={subtitle}
-                description={description}
-                imageLink={fileName}
-              />
-            </SwiperSlide>
-          ))}
-        </Swiper>
+        <div className="md:px-40">
+          <Swiper
+            navigation={true}
+            modules={[Navigation]}
+            className="mySwiper"
+            slidesPerView={1}
+            spaceBetween={5}
+            // Responsive breakpoints
+            breakpoints={{
+              // when window width is >= 320px
+              320: {
+                slidesPerView: 1,
+                spaceBetween: 5,
+              },
+              // when window width is >= 480px
+              480: {
+                slidesPerView: 1,
+                spaceBetween: 10,
+              },
+              620: {
+                slidesPerView: 2,
+                spaceBetween: 15,
+              },
+              // when window width is >= 640px
+              840: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+              },
+              1024: {
+                slidesPerView: 3,
+                spaceBetween: 10,
+              },
+            }}
+          >
+            {speakers.map(
+              (
+                { name, subtitle, description, fileName, githubLink, linkedinLink, websiteLink },
+                idx,
+              ) => (
+                <SwiperSlide key={idx}>
+                  <KeynoteSpeaker
+                    name={name}
+                    subtitle={subtitle}
+                    description={description}
+                    imageLink={fileName}
+                    githubLink={githubLink}
+                    linkedinLink={linkedinLink}
+                    websiteLink={websiteLink}
+                  />
+                </SwiperSlide>
+              ),
+            )}
+          </Swiper>
+        </div>
       </section>
     )
   );
--- components/homeComponents/KeynoteSpeaker.tsx
+++ components/homeComponents/KeynoteSpeaker.tsx
@@ -13,6 +13,7 @@ export default function KeynoteSpeaker(props) {
   const [imageLink, setImageLink] = useState();
 
   useEffect(() => {
+    console.log(props);
     if (props.imageLink !== undefined) {
       const storageRef = firebase.storage().ref();
       storageRef
@@ -28,31 +29,31 @@ export default function KeynoteSpeaker(props) {
   }, []);
 
   return (
-    <div className="flex flex-col items-center w-72 md:w-80 h-[350px] md:h-80 rounded-3xl relative overflow-hidden border-2 border-gray-300 font-inter">
+    <div className="flex flex-col items-center w-72 md:w-[360px] h-[350px] md:h-[420px] rounded-3xl relative overflow-hidden border-2 border-gray-300 font-inter bg-white">
       <div className="rounded-t-sm">
         {props.imageLink !== undefined && imageLink !== undefined && (
-          <Image src={imageLink} width={350} height={200} objectFit="cover" alt="" />
+          <Image src={imageLink} width={360} height={240} objectFit="cover" alt="" />
         )}
       </div>
-      <div className="flex-col items-center justify-center w-full h-80 absolute translate-y-44 pt-3">
-        <div className="px-8 md:px-4">
-          <div className="text-lg md:text-md font-bold pb-1">{props.name}</div>
-          <div className="text-md md:text-sm font-normal pb-2 md:pb-1">{props.subtitle}</div>
-          <div className="text-xs md:text-[8pt] pb-6 md:pb-2">{props.description}</div>
-          <div className="flex space-x-3">
-            <div className="flex items-center justify-center w-8 h-8 rounded-full bg-[#9CA6FF]">
-              <a href="https://www.linkedin.com" target="_blank" rel="noreferrer">
-                <LinkedInIcon style={{ fontSize: 25, color: 'white' }} />
+      <div className="flex-col items-center justify-center w-full h-80 absolute translate-y-48 md:translate-y-60 pt-3">
+        <div className="px-8 md:px-6">
+          <div className="text-xl font-semibold pb-1">{props.name}</div>
+          <div className="text-md font-normal pb-1 text-[#3E3E59]">{props.subtitle}</div>
+          <div className="text-xs pb-2 text-[#5F6980]">{props.description}</div>
+          <div className="flex space-x-3 md:pt-2">
+            <div className="flex items-center justify-center w-10 h-10 rounded-full bg-[#9CA6FF]">
+              <a href={props.linkedinLink} target="_blank" rel="noreferrer">
+                <LinkedInIcon style={{ fontSize: 29, color: 'white' }} />
               </a>
             </div>
-            <div className="flex items-center justify-center w-8 h-8 rounded-full bg-[#9CA6FF]">
-              <a href="https://www.github.com" target="_blank" rel="noreferrer">
-                <GitHubIcon style={{ fontSize: 25, color: 'white' }} />
+            <div className="flex items-center justify-center w-10 h-10 rounded-full bg-[#9CA6FF]">
+              <a href={props.githubLink} target="_blank" rel="noreferrer">
+                <GitHubIcon style={{ fontSize: 29, color: 'white' }} />
               </a>
             </div>
-            <div className="flex items-center justify-center w-8 h-8 rounded-full bg-[#9CA6FF]">
-              <a href="https://www.google.com" target="_blank" rel="noreferrer">
-                <LanguageIcon style={{ fontSize: 25, color: 'white' }} />
+            <div className="flex items-center justify-center w-10 h-10 rounded-full bg-[#9CA6FF]">
+              <a href={props.websiteLink} target="_blank" rel="noreferrer">
+                <LanguageIcon style={{ fontSize: 29, color: 'white' }} />
               </a>
             </div>
           </div>
