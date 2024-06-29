97b9a6d6605ec521f73afa52ff467b7d791fa7a3
Mike Nguyen <mikenguyen7268@gmail.com>
2024-06-29 11:07:19 -0500

    feat: Fixed long horizontal scrollbar

--- components/homeComponents/HomePrizes.tsx
+++ components/homeComponents/HomePrizes.tsx
@@ -9,22 +9,20 @@ interface HomePrizesProps {
 
 export default function HomePrizes({ prizes }: HomePrizesProps) {
   return (
-    <section className="w-full">
+    <section className="w-full overflow-hidden">
       {/* Banner */}
-      <div
-        className="-mx-4 my-6 -rotate-[4deg] bg-[#7B81FF] text-white p-3 w-lvw"
-        style={{
-          textOverflow: 'ellipsis',
-          whiteSpace: 'nowrap',
-        }}
-      >
-        {Array.apply(null, Array(200))
-          .map(() => 'PRIZES')
-          .join(' ')}
+      <div className="-mx-4 mt-[120px] -rotate-[7deg] bg-[#7B81FF] text-white py-[0.35rem] w-[2000px] overflow-hidden line-clamp-1">
+        <p>
+          {Array.apply(null, Array(200))
+            .map(() => 'PRIZES')
+            .join('  ')}
+        </p>
       </div>
       {/* Component */}
       <div className="md:py-12 py-6 xl:w-9/10 w-11/12 m-auto">
-        <h1 className="mt-[100px] text-3xl text-center font-bold text-[#05149C]">Prizes</h1>
+        <h1 className="font-[Fredoka] mt-[100px] text-3xl text-center font-bold text-[#05149C]">
+          Prizes
+        </h1>
         <p className="text-center">Potential prize that participants can win!</p>
         <div className="md:grid md:grid-cols-3 flex flex-col gap-x-6 gap-y-[140px] mt-6">
           {prizes.map((prize, idx) => (
