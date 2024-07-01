16730d236b32752b589943b3348d4e8601c2dc40
Mike Nguyen <mikenguyen7268@gmail.com>
2024-06-30 23:04:17 -0500

    refactor: Added right padding

--- components/homeComponents/HomeChallengeCard.tsx
+++ components/homeComponents/HomeChallengeCard.tsx
@@ -7,17 +7,19 @@ export default function HomeChallengesCard(props: { challenge: Challenge; blockT
         <div className={`bg-[#C1C8FF] ${borderConfiguration[props.blockType]} w-4/5 h-[200px]`}>
           &nbsp;
         </div>
-        {/* Challenge Name */}
-        <h1 className="font-nunito text-2xl font-bold mt-4">
-          {props.challenge.title.toUpperCase()}
-        </h1>
-        {/* Company Name */}
-        <h1 className="font-nunito text-xl text-[#05149C] font-bold my-4">
-          {props.challenge.organization}
-        </h1>
-        {/* Description */}
-        <div className="mb-8 max-w-fit">
-          <p className="text-md line-clamp-5 text-balance">{props.challenge.description}</p>
+        <div className="pr-[60px]">
+          {/* Challenge Name */}
+          <h1 className="font-nunito text-2xl font-bold mt-4">
+            {props.challenge.title.toUpperCase()}
+          </h1>
+          {/* Company Name */}
+          <h1 className="font-nunito text-xl text-[#05149C] font-bold my-4">
+            {props.challenge.organization}
+          </h1>
+          {/* Description */}
+          <div className="mb-8 max-w-fit">
+            <p className="text-md line-clamp-5 text-balance">{props.challenge.description}</p>
+          </div>
         </div>
       </div>
     </div>
