bd41acd181205dd742ac42e1c28bc652e55125aa
Reyer Chu <reyerchu@defintek.io>
2025-10-28 01:16:57 +0800

    feat: add NTHUBC to special thanks section

--- components/homeComponents/HomeOrganizers.tsx
+++ components/homeComponents/HomeOrganizers.tsx
@@ -321,6 +321,23 @@ export default function HomeOrganizers() {
                 />
               </a>
             </div>
+            <div className="flex items-center justify-center">
+              <a
+                href="https://linktr.ee/nthubc"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="transition-opacity hover:opacity-80"
+              >
+                <Image
+                  src="/sponsor-media/others/nthubc.jpg"
+                  alt="NTHUBC"
+                  width={340}
+                  height={150}
+                  className="w-auto object-contain"
+                  style={{ height: '120px' }}
+                />
+              </a>
+            </div>
           </div>
         </div>
       </div>
